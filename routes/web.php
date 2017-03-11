967d65f4fed74da8908a1b369eb5379034e856f7
Ed Steinbauer <steinbauer@fas.harvard.edu>
2017-03-11 14:56:01 -0500

    Practice controller setup, evironments, packages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+if (config('app.env') == 'local') {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+
 Route::get('/books', 'BookController@index');
 
 Route::get('/books/{title?}', 'BookController@view');
+
+/**
+* Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
 
 Route::get('/', 'WelcomeController');
